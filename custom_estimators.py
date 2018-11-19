updated make_csv_dataset for multi output, added #hidden units computation to gui, fixed some issues when loading from saved config

custom_estimators.py
@@ -237,18 +237,15 @@ def dnn(net, params, mode):
         'scale_l2': params['l2_regularization']
     }
     initializer_name = params['kernel_initializer']['name']
+    kernel_initializer = None
 
     if 'params' in params['kernel_initializer']:
         dict_param_initializer = dic_initializer_param(initializer_name, params['kernel_initializer']['params'])
 
         kernel_initializer = getattr(tf.initializers, initializers_opts[initializer_name])(**dict_param_initializer)
-
-        model = _DNNModel(get_num_outputs(params), params['hidden_units'], params['activation_fn'], dropout,
-                          params['batch_norm'], kernel_regularizer_params=regularizer_params,
-                          kernel_initializer=kernel_initializer)
-    else:
-        model = _DNNModel(get_num_outputs(params), params['hidden_units'], params['activation_fn'], dropout,
-                          params['batch_norm'], kernel_regularizer_params=regularizer_params)
+    model = _DNNModel(get_num_outputs(params), params['hidden_units'], params['activation_fn'], dropout,
+                      params['batch_norm'], kernel_regularizer_params=regularizer_params,
+                      kernel_initializer=kernel_initializer)
 
     return model(net, mode)
 

feature_selection.py
@@ -74,7 +74,6 @@ class FeatureSelection:
                 self.categorical_columns.append(col)
                 self.int_columns.remove(col)
 
-
     def group_by(self, datatypes):
         columns_types_dict = dict(zip(self.df.columns, datatypes))
         v = defaultdict(list)
@@ -105,8 +104,7 @@ class FeatureSelection:
                 norm_fn = lambda x: (x - mean) / stdv
             numerical_features.append(tf.feature_column.numeric_column(key, normalizer_fn=norm_fn))
 
-
-        #numerical_features = [tf.feature_column.numeric_column(key, normalizer_fn=norm_fn) for key in feature_types['numerical']]
+        # numerical_features = [tf.feature_column.numeric_column(key, normalizer_fn=norm_fn) for key in feature_types['numerical']]
 
         range_features = [tf.feature_column.indicator_column(
             tf.feature_column.categorical_column_with_identity(key, self.df[key].max() + 1)) for key in

make_csv_dataset.py
@@ -17,12 +17,12 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
-from tensorflow.contrib.data.python.ops.readers import dataset_ops, dtypes, constant_op, interleave_ops, \
+from tensorflow.python.data.experimental.ops.readers import dataset_ops, dtypes, constant_op, interleave_ops, \
     _get_sorted_col_indices, CsvDataset, _maybe_shuffle_and_repeat
-from tensorflow.contrib.data.python.ops.readers import _infer_column_defaults, _infer_column_names, _get_file_names
+from tensorflow.python.data.experimental.ops.readers import _infer_column_defaults, _infer_column_names, _get_file_names
 import tensorflow as tf
 import collections
-from tensorflow.python.feature_column.feature_column import _IndicatorColumn
+from tensorflow.python.data.experimental.ops import optimization
 
 _ACCEPTABLE_CSV_TYPES = (dtypes.float32, dtypes.float64, dtypes.int32,
                          dtypes.int64, dtypes.string)
@@ -43,12 +43,11 @@ def make_csv_dataset_multiple_output(
         shuffle=True,
         shuffle_buffer_size=10000,
         shuffle_seed=None,
-        prefetch_buffer_size=1,
+        prefetch_buffer_size=optimization.AUTOTUNE,
         num_parallel_reads=1,
-        num_parallel_parser_calls=2,
         sloppy=False,
         num_rows_for_inference=100,
-        feature_columns=None,
+        compression_type=None,
         input_name=None
 ):
     # Create dataset of all matching filenames
@@ -66,10 +65,6 @@ def make_csv_dataset_multiple_output(
         column_names = _infer_column_names(filenames, field_delim, use_quote_delim)
     if len(column_names) != len(set(column_names)):
         raise ValueError("Cannot have duplicate column names.")
-
-    if feature_columns is not None:
-        if input_name is None:
-            raise ValueError("Missing input name for feature columns.")
 
     if select_columns is not None:
         select_columns = _get_sorted_col_indices(select_columns, column_names)
@@ -109,7 +104,9 @@ def make_csv_dataset_multiple_output(
             use_quote_delim=use_quote_delim,
             na_value=na_value,
             select_cols=select_columns,
-            header=header)
+            header=header,
+            compression_type=compression_type,
+        )
 
     def map_fn(*columns):
         """Organizes columns into a features dictionary.
@@ -125,11 +122,8 @@ def make_csv_dataset_multiple_output(
             labels = []
             for l in label_names:
                 labels.append(features.pop(l))
-            if feature_columns is not None:
-                features = {input_name: tf.feature_column.input_layer(features, feature_columns)}
             return features, tf.stack(labels, 1)
-        if feature_columns is not None:
-            features = {input_name: tf.feature_column.input_layer(features, feature_columns)}
+
         return features
 
     # Read files sequentially (if num_parallel_reads=1) or in parallel
@@ -141,133 +135,15 @@ def make_csv_dataset_multiple_output(
         dataset, num_epochs, shuffle, shuffle_buffer_size, shuffle_seed)
 
     # Apply batch before map for perf, because map has high overhead relative
-    # to the size of the computation in each map
-    dataset = dataset.batch(batch_size=batch_size)
-    dataset = dataset.map(map_fn, num_parallel_calls=num_parallel_parser_calls)
-    dataset = dataset.prefetch(prefetch_buffer_size)
-
-    return dataset
-
-
-def make_csv_dataset(
-        file_pattern,
-        batch_size,
-        column_names=None,
-        column_defaults=None,
-        label_name=None,
-        select_columns=None,
-        field_delim=",",
-        use_quote_delim=True,
-        na_value="",
-        header=True,
-        num_epochs=None,
-        shuffle=True,
-        shuffle_buffer_size=10000,
-        shuffle_seed=None,
-        prefetch_buffer_size=1,
-        num_parallel_reads=1,
-        num_parallel_parser_calls=2,
-        sloppy=False,
-        num_rows_for_inference=100,
-        feature_columns=None,
-        input_name=None
-
-):
-    # Create dataset of all matching filenames
-    filenames = _get_file_names(file_pattern, False)
-    dataset = dataset_ops.Dataset.from_tensor_slices(filenames)
-    if shuffle:
-        dataset = dataset.shuffle(len(filenames), shuffle_seed)
-
-    # Clean arguments; figure out column names and defaults
-
-    if column_names is None:
-        if not header:
-            raise ValueError("Cannot infer column names without a header line.")
-        # If column names are not provided, infer from the header lines
-        column_names = _infer_column_names(filenames, field_delim, use_quote_delim)
-    if len(column_names) != len(set(column_names)):
-        raise ValueError("Cannot have duplicate column names.")
-
-    if select_columns is not None:
-        select_columns = _get_sorted_col_indices(select_columns, column_names)
-
-    if column_defaults is not None:
-        column_defaults = [
-            constant_op.constant([], dtype=x) if x in _ACCEPTABLE_CSV_TYPES else x
-            for x in column_defaults
-        ]
-    else:
-        # If column defaults are not provided, infer from records at graph
-        # construction time
-        column_defaults = _infer_column_defaults(
-            filenames, len(column_names), field_delim, use_quote_delim, na_value,
-            header, num_rows_for_inference, select_columns)
-
-    if select_columns is not None and len(column_defaults) != len(select_columns):
-        raise ValueError(
-            "If specified, column_defaults and select_columns must have same "
-            "length."
-        )
-    if select_columns is not None and len(column_names) > len(select_columns):
-        # Pick the relevant subset of column names
-        column_names = [column_names[i] for i in select_columns]
-
-    if label_name is not None and label_name not in column_names:
-        raise ValueError("`label_name` provided must be one of the columns.")
-
-    def filename_to_dataset(filename):
-        return CsvDataset(
-            filename,
-            record_defaults=column_defaults,
-            field_delim=field_delim,
-            use_quote_delim=use_quote_delim,
-            na_value=na_value,
-            select_cols=select_columns,
-            header=header)
-
-    def retrieve_target(fc, target):
-        for x in fc:
-            if type(x) == _IndicatorColumn:
-                if x[0].key == target:
-                    return fc.pop(fc.index(x))
-            else:
-                if x.key == target:
-                    return fc.pop(fc.index(x))
-
-    def map_fn(*columns):
-        """Organizes columns into a features dictionary.
-        Args:
-          *columns: list of `Tensor`s corresponding to one csv record.
-        Returns:
-          An OrderedDict of feature names to values for that particular record. If
-          label_name is provided, extracts the label feature to be returned as the
-          second element of the tuple.
-        """
-        features = collections.OrderedDict(zip(column_names, columns))
-        if label_name is not None:
-            label = features.pop(label_name)
-            if feature_columns is not None:
-                feature_label = retrieve_target(feature_columns, label_name)
-                features = {input_name: tf.feature_column.input_layer(features, feature_columns)}
-                label = tf.feature_column.input_layer({label_name: label}, [feature_label])
-            return features, label
-        if feature_columns is not None:
-            features = {input_name: tf.feature_column.input_layer(features, feature_columns)}
-        return features
-
-    # Read files sequentially (if num_parallel_reads=1) or in parallel
-    dataset = dataset.apply(
-        interleave_ops.parallel_interleave(
-            filename_to_dataset, cycle_length=num_parallel_reads, sloppy=sloppy))
-
-    dataset = _maybe_shuffle_and_repeat(
-        dataset, num_epochs, shuffle, shuffle_buffer_size, shuffle_seed)
-
-    # Apply batch before map for perf, because map has high overhead relative
-    # to the size of the computation in each map
-    dataset = dataset.batch(batch_size=batch_size)
-    dataset = dataset.map(map_fn, num_parallel_calls=num_parallel_parser_calls)
+    # to the size of the computation in each map.
+    # NOTE(mrry): We set `drop_remainder=True` when `num_epochs is None` to
+    # improve the shape inference, because it makes the batch dimension static.
+    # It is safe to do this because in that case we are repeating the input
+    # indefinitely, and all batches will be full-sized.
+    dataset = dataset.batch(batch_size=batch_size,
+                            drop_remainder=num_epochs is None)
+    dataset = dataset_ops.MapDataset(
+        dataset, map_fn, use_inter_op_parallelism=False)
     dataset = dataset.prefetch(prefetch_buffer_size)
 
     return dataset

test/test_csv_reader.py
@@ -12,6 +12,7 @@ tf.enable_eager_execution()
 # CONFIG_FILE = "../config/default.ini"
 CONFIG_FILE = "data_test/iris_config.ini"
 
+
 @pytest.fixture
 def config():
     # return config_reader.read_config(sys_ops.abs_path_of(CONFIG_FILE))
@@ -20,7 +21,8 @@ def config():
 
 @pytest.fixture
 def train_reader(config):
-    defaults = {'sepal_length': '5.8', 'sepal_width': '3', 'petal_length': '4.35', 'petal_width': '1.3', 'class': 'Iris-setosa'}
+    defaults = {'sepal_length': '5.8', 'sepal_width': '3', 'petal_length': '4.35', 'petal_width': '1.3',
+                'class': 'Iris-setosa'}
     dtypes = {'numerical': ['sepal_length', 'sepal_width', 'petal_length', 'petal_width'], 'categorical': ['class']}
     return TrainCSVReader(config, defaults, dtypes, 'class')
 
@@ -52,6 +54,7 @@ def test_feature_columns(train_reader):
     print(a, type(a), a.shape)
     np.testing.assert_array_equal(train_reader._feature_names(),
                                   ['sepal_length', 'sepal_width', 'petal_length', 'petal_width'])
+
 
 def test_unique_values(train_reader):
     np.testing.assert_array_equal(train_reader.label_unique_values(), ['setosa', 'versicolor', 'virginica'])