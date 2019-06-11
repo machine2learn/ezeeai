c0db0b0a97d46b5690a160fc40ef9fd9ceb1688b
aracelicanadas <araceli@machine2leearn.nl>
2019-06-11 11:24:52 +0200

    update deployment

--- deployment.sh
+++ deployment.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-docker pull tensorflow/serving:1.8.0
+docker pull tensorflow/serving:1.13.1
 
-nohup docker run -p 8501:8501 --mount type=bind,source=$PWD,target=/models/ -e MODEL_NAME=model_name -t tensorflow/serving:1.8.0 1>log 2>err &
+nohup docker run -p 8501:8501 --mount type=bind,source=$PWD,target=/models/ -e MODEL_NAME=model_name -t tensorflow/serving:1.13.1 1>log 2>err &
 
 
