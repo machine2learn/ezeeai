53a0d965c503878abaf31a8d4127260f828ec5da
aracelicanadas <araceli@machine2leearn.nl>
2019-06-11 10:46:05 +0200

    Revert "fix param error"
    This reverts commit f97475076642522d0c2b6356517e416a6c9abcc9.

--- ezeeai/core/session.py
+++ ezeeai/core/session.py
@@ -209,7 +209,6 @@ class Session:
             return True
         return False
 
-
     def check_log_fp(self, all_params_config):
         logfile = os.path.join(all_params_config['PATHS']['log_dir'], 'tensorflow.log')
         try:
