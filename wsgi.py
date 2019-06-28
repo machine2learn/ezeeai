25cd19b848fef240d5e91d17144ac7cd7261430c
yoelsb <yoel.sb90@gmail.com>
2019-06-28 15:35:13 +0200

    5 to 1

--- wsgi.py
+++ wsgi.py
@@ -8,7 +8,7 @@ def open_browser():
 
 
 if __name__ == "__main__":
-    Timer(5, open_browser).start()
+    Timer(1, open_browser).start()
     app.run(debug=False,
             threaded=True,
             host=appConfig.host(),
